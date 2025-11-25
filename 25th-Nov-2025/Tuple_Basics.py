1abe9f8e40e7bf43a4362d58e3679ebb4c010a88
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-11-25 12:34:53 +0530

    Update Tuple_Basics.py

--- 25th-Nov-2025/Tuple_Basics.py
+++ 25th-Nov-2025/Tuple_Basics.py
@@ -11,3 +11,13 @@ print(mixed)
 print(numbers[0])
 print(names[-1])
 print(names[1])
+
+#count and index
+print(numbers.count(2))
+print(names.index("James"))
+
+#print operations
+print(numbers[1:4])
+print(numbers[:3])
+print(numbers[::-1])
+print(numbers[2:])
