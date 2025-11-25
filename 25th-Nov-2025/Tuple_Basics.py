552f1d6e15235e638879ecb10c7b10ecccfea40f
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-11-25 12:30:00 +0530

    Create Tuple_Basics.py

---
+++ 25th-Nov-2025/Tuple_Basics.py
@@ -0,0 +1,13 @@
+#Tuple is immutable meaning it cant be changed
+
+numbers=(1,2,3)
+names=("Bishal","James","Subham","Niket","Asad")
+mixed=(10,"Hello",3.5,True)
+
+print(numbers)
+print(names)
+print(mixed)
+
+print(numbers[0])
+print(names[-1])
+print(names[1])
