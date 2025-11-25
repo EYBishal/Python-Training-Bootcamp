78ae6323a321a228db69b98b65553aefc9ad40ba
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-11-25 12:47:34 +0530

    Update Tuple_Basics.py

--- 25th-Nov-2025/Tuple_Basics.py
+++ 25th-Nov-2025/Tuple_Basics.py
@@ -21,3 +21,23 @@ print(numbers[1:4])
 print(numbers[:3])
 print(numbers[::-1])
 print(numbers[2:])
+
+#packing and unpacking
+data=10,20,30
+a,b,c=data
+print(a,b,c)
+
+#Nested tuple
+emp=("John",32,("Kolkata","India"))
+print(emp[2][0])
+
+#Max and Min using Tuple
+numbers=(10,20,30,40,50)
+max=-100
+min=100
+for n in numbers:
+    if n>max:
+        max=n
+    if n<min:
+        min=n
+print(max,min)
