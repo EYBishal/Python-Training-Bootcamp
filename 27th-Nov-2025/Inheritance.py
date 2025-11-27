3d84c3e90060e264eb24715b6e1833033d3745a8
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-11-27 09:33:21 +0530

    Update Inheritance.py

--- 27th-Nov-2025/Inheritance.py
+++ 27th-Nov-2025/Inheritance.py
@@ -1,7 +1,8 @@
 class Animal:
     def speak(self):
         print("Animal makes a sound")
-
+#inheritance example
+#here the animal class is passed to dog so that it can acces the methods of parent class
 class Dog(Animal):
     def bark(self):
         print("Dog makes a sound")
@@ -9,3 +10,19 @@ class Dog(Animal):
 d=Dog()
 d.speak()
 d.bark()
+
+########################################################
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+#super() keyword is use to acces the constructor of the parent as child uses it from inheritance
+
+class child(Person):
+    def __init__(self, name, age):
+        super().__init__(name)
+        self.age = age
+
+e=child("Bishal",10)
+print(e.name,e.age)
