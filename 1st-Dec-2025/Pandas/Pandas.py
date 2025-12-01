fe9bf834da1699a0979d2082e94c9bf53ae9e0c8
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-12-01 15:03:56 +0530

    Update Pandas.py

--- 1st-Dec-2025/Pandas/Pandas.py
+++ 1st-Dec-2025/Pandas/Pandas.py
@@ -32,3 +32,72 @@ df=pd.DataFrame(data)
 
 df.to_csv("students.csv",index=False)
 print("file Created")
+
+
+####################################################
+
+import pandas as pd
+
+df=pd.read_csv("students.csv")
+
+print(df)
+
+##############################################
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["Aisha", "Rahul", "John", "Neha", "Imran"],
+    "Marks": [85, 92, 78, 65, 55],
+    "City": ["Mumbai", "Delhi", "Chennai", "Bangalore", "Hyderabad"],
+    "Age": [22, 25, 23, 21, 24]
+})
+
+print(df)
+
+print(df.head(2))
+print(df.tail(2))
+print(df.shape)
+print(df.columns)
+print(df.describe())
+
+###################################################
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["Aisha", "Rahul", "John", "Neha", "Imran"],
+    "Marks": [85, 92, 78, 65, 55],
+    "City": ["Mumbai", "Delhi", "Chennai", "Bangalore", "Hyderabad"],
+    "Age": [22, 25, 23, 21, 24]
+})
+
+print(df)
+
+
+df["result"] =df["Marks"].apply(lambda x: "Pass" if x > 70 else "Fail")
+print(df)
+
+sorted_df=df.sort_values(by="Marks", ascending=False)
+print(sorted_df)
+
+########################################################
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["Aisha", "Rahul", "John", "Neha", "Imran"],
+    "Marks": [85, 92, 78, 65, 55],
+    "City": ["Mumbai", "Delhi", "Chennai", "Bangalore", "Hyderabad"],
+    "Age": [22, 25, 23, 21, 24]
+})
+
+print(df)
+
+df2=df.copy()
+df2.loc[2,"City"]= None
+print(df2)
+print(df2.isnull().sum())
+
+df2_filled =df2.fillna("Unknown")
+print(df2_filled)
