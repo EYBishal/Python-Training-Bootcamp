5ad4a783becda319ae2925c83b49d7e1bff29c90
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-12-01 15:27:27 +0530

    Update Pandas.py

--- 1st-Dec-2025/Pandas/Pandas.py
+++ 1st-Dec-2025/Pandas/Pandas.py
@@ -101,3 +101,23 @@ print(df2.isnull().sum())
 
 df2_filled =df2.fillna("Unknown")
 print(df2_filled)
+
+####################################################
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name": ["Aisha", "Rahul", "John", "Neha", "Imran"],
+    "Marks": [85, 92, 78, 65, 55],
+    "City": ["Mumbai", "Delhi", "Chennai", "Bangalore", "Hyderabad"],
+    "Age": [22, 25, 23, 21, 24]
+})
+
+print(df)
+
+
+city_count=df.groupby("City")["Name"].count()
+print(city_count)
+
+avg_marks=df.groupby("City")["Marks"].mean()
+print(avg_marks)
