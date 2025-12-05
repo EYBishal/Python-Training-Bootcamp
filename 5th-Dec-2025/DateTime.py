6894bc4276da57bcaf4a253cbf34e4c4f7dff3ac
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-12-05 11:02:13 +0530

    Update DateTime.py

--- 5th-Dec-2025/DateTime.py
+++ 5th-Dec-2025/DateTime.py
@@ -16,3 +16,20 @@ print(dob)
 now=datetime.now()
 form=now.strftime("%d-%m-%y %H:%M:%S")
 print(form) 
+
+
+today=date.today()
+next_week=today + timedelta(days=7)
+yesterday=today - timedelta(days=1)
+print(today)
+print(next_week)
+print(yesterday)
+
+start=date(2024,1,1)
+end=date(2024,12,31)
+diff=end-start
+print(diff.days)
+
+
+dt=datetime.combine(date(2024,12,31),time(12,15,0))
+print(dt)
