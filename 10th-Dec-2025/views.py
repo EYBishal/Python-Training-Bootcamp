9dc136e199073db4401226e056f660639767d40e
EYBishal <Bishal.Bhattacharjee@gds.ey.com>
2025-12-10 16:53:59 +0530

    Update views.py

--- 10th-Dec-2025/views.py
+++ 10th-Dec-2025/views.py
@@ -54,3 +54,48 @@ def api_view(request):
         return JsonResponse({"message": "PUT REQUEST"})
     elif request.method == "DELETE":
         return JsonResponse({"message": "DELETE REQUEST"})
+        
+--------------------------------       -- -----------------------------------
+-------------------------------        --------------------------------------
+
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Vehicle
+import json
+@csrf_exempt
+def vehicle(request):
+    if request.method == 'GET':
+        data = list(Vehicle.objects.values())
+        return JsonResponse(data, safe=False)
+
+    if request.method == 'POST':
+        body=json.loads(request.body)
+        Vehicle.objects.create(
+            number_plate=body['number_plate'],
+            vehicle_type=body['vehicle_type'],
+            manufacturer=body['manufacturer'],
+            year=body['year'],
+        )
+        return JsonResponse({'success':True})
+
+    if request.method == 'PUT':
+        try:
+            body = json.loads(request.body.decode('utf-8'))
+            p = Vehicle.objects.get(id=body['id'])
+            p.number_plate = body['number_plate']
+            p.vehicle_type = body['vehicle_type']
+            p.manufacturer = body['manufacturer']
+            p.year = body['year']
+
+            p.save()
+            return JsonResponse({'success': True})
+        except Exception as e:
+            print(e)
+    if request.method == 'DELETE':
+        body=json.loads(request.body)
+        p=Vehicle.objects.get(id=body['id'])
+        p.delete()
+        return JsonResponse({'success':True})
+
+    return JsonResponse({'success':False})
